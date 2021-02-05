f4c44e62a50315bf2444ab9b7aee48d59187eba2
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-05 10:56:33 -0800

    add red stage test for age in mercury years

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -5,4 +5,9 @@ describe('Person', () => {
     const person = new Person(23);
     expect(person.age).toEqual(23);
   });
+
+  test("should correctly return users age in Mercury years", () => {
+    const person = new Person(23);
+    expect(person.getMercuryAge()).toEqual(23 * 0.24) 
+  })
 });
