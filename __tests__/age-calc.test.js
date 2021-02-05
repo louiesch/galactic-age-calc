2f61ce8ce9a2df25018cc840f74595d2cc6389ef
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-05 12:56:34 -0800

    refactor tests to use beforeEach() block

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -1,18 +1,22 @@
 import Person from '../src/age-calc.js';
 
 describe('Person', () => {
+
+  let person;
+
+  beforeEach(() => {
+    person = new Person(23);
+  });
+
   test('should correctly return users age in earth years', () => {
-    const person = new Person(23);
     expect(person.age).toEqual(23);
   });
 
   test("should correctly return users age in Mercury years", () => {
-    const person = new Person(23);
     expect(person.getMercuryAge()).toEqual(23 / 0.24);
   });
   
   test("should correctly return users age in Venus years", () => {
-    const person = new Person(23);
     expect(person.getVenusAge()).toEqual(23 / 0.62);
   });
 });
