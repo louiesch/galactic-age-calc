a65c4af7ad58cf910a44e1d0d7a4ca4ac10db67d
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-05 08:43:19 -0800

    write first test to return users earth age

---
+++ __tests__/age-calc.test.js
@@ -0,0 +1,8 @@
+import { TestScheduler } from 'jest';
+import ageCalc from '../src/age-calc.js';
+
+describe('ageCalc', () => {
+  test('should correctly return users age in earth years', () => {
+    const person = new Person()
+  })
+})
